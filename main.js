d78bec3d90756cc34a726eeaba3ed38bf032ba8a
briancosentino <cosentinobrian@gmail.com>
2025-02-24 15:57:16 +0100

    add html

---
+++ main.js
@@ -0,0 +1,40 @@
+const rowEl = document.querySelector('.row')
+const teamMembers = [
+    {
+      name: "Marco Bianchi",
+      job: "Designer",
+      email: "marcobianchi@team.com",
+      img: "marco-bianchi.jpg"
+    },
+    {
+      name: "Laura Rossi",
+      job: "Front-end Developer",
+      email: "laurarossi@team.com",
+      img: "laura-rossi.jpg"
+    },
+    {
+      name: "Giorgio Verdi",
+      job: "Back-end Developer",
+      email: "giorgioverdi@team.com",
+      img: "giorgio-verdi.jpg"
+    },
+    {
+      name: "Marta Ipsum",
+      job: "SEO Specialist",
+      email: "martarossi@team.com",
+      img: "marta-ipsum.jpg"
+    },
+    {
+      name: "Roberto Lorem",
+      job: "SEO Specialist",
+      email: "robertolorem@team.com",
+      img: "roberto-lorem.jpg"
+    },
+    {
+      name: "Daniela Amet",
+      job: "Analyst",
+      email: "danielaaamet@team.com",
+      img: "daniela-amet.jpg"
+    }
+  ];
+  
